e572419158ee5a90fbfa1b94948f2e9654f76199
Dimitri <dimitri.oliveira@dcomp.ufs.br>
2025-07-18 01:58:09 -0300

    Update Resolução funcoes_de_alta_ordem.js

--- Resolução funcoes_de_alta_ordem.js
+++ Resolução funcoes_de_alta_ordem.js
@@ -7,4 +7,4 @@ const totalLista = total(lista)
 const oQueFalta = (t) => (e) => (t-e) + e //Função utilizada para somar a diferença entre o total e o elemento da lista.
 
 const resultado = lista.map(oQueFalta(totalLista)) //O resultado retorna uma lista com 5 elementos iguais ao resultado da soma dos elementos anteriores.
-console.log (resultado)
+console.log (resultado);
